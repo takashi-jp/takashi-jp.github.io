251c167e9069060e9de5e55095b778c12a2cf836
Takashi Nakano <45597684+takashi-jp@users.noreply.github.com>
2026-02-11 13:34:32 +0900

    Update answers_no and answers_yes data

--- public/js/script.js
+++ public/js/script.js
@@ -3,7 +3,7 @@ const answers_no = {
         "No",
         "Are you sure?",
         "Are you really sure??",
-        "Are you really realy sure???",
+        "Are you really really sure???",
         "Think again?",
         "Don't believe in second chances?",
         "Why are you being so cold?",
@@ -16,47 +16,29 @@ const answers_no = {
         "I am begging you to stop!",
         "Ok, Let's just start over.."
     ],
-    french: [
-        "Non",
-        "Tu es sûr ?",
-        "Tu es vraiment sûr ??",
-        "Tu es vraiment vraiment sûr ???",
-        "Réfléchis encore?",
-        "Tu ne crois pas aux deuxièmes chances ?",
-        "Pourquoi tu es si froid?",
-        "Peut-être, on peut en parler ?",
-        "Je ne vais pas demander encore une fois!",
-        "D'accord, maintenant ca me fait mal!",
-        "Tu es juste méchant!",
-        "Pourquoi tu me fais ça?",
-        "Donnez-moi une chance plz!",
-        "Je te supplie d'arrêter!",
-        "D'accord, recommençons.."
-    ],
-    thai: [
-        "ไม่อ่ะ",
-        "แน่ใจจริงๆหรอคะ?",
-        "แน่ใจจริงๆ จริงๆนะคะ?",
-        "อย่าบอกนะว่านี่แน่ใจสุดๆแล้วจริงๆ ?",
-        "ลองคิดดูอีกทีหน่อยสิคะ..",
-        "ขอโอกาศที่สองทีค่ะ..",
-        "อย่าเย็นชาสิคะ กระซิกๆ",
-        "ขอร้องนะคะ",
-        "น้าาาๆๆๆๆๆ",
-        "เราจะร้องไห้เอานะ กระซิกๆ",
-        "จะเอางี้ๆจริงหรอคะ",
-        "ฮือออออ",
-        "ขอโอกาศครั้งที่สองที่ค่ะ!",
-        "ขอร้องละค่าาา",
-        "โอเคค่ะ.. งั้นเริ่มใหม่ !"
+    japanese: [
+        "いいえ",
+        "本当にそれでいいんですか?",
+        "本当に本当ですか??",
+        "そんなに確信しているのですか???",
+        "もう一度考え直してみませんか?",
+        "二度目のチャンスを信じてみませんか?",
+        "なぜそんなに冷たいのですか?",
+        "話し合うことはできませんか?",
+        "もうこれ以上お願いしません!",
+        "これ以上は傷つきます!",
+        "意地悪しないでください!",
+        "どうしてこんなことをするんですか?",
+        "チャンスをください!",
+        "お願いです、やめてください!",
+        "もう一度、最初から始めましょう.."
     ]
 };
 
-answers_yes = {
-    "english": "Yes",
-    "french": "Oui",
-    "Thailand": "เย่ คืนดีกันแล้วน้า"
-}
+const answers_yes = {
+    english: "Yes",
+    japanese: "はい"
+};
 
 let language = "english"; // Default language is English
 const no_button = document.getElementById('no-button');
@@ -66,21 +48,18 @@ let size = 50;
 let clicks = 0;
 
 no_button.addEventListener('click', () => {
-    // Change banner source
     let banner = document.getElementById('banner');
     if (clicks === 0) {
         banner.src = "public/images/no.gif";
         refreshBanner();
     }
     clicks++;
-    // increase button height and width gradually to 250px
-    const sizes = [40, 50, 30, 35, 45]
+    const sizes = [40, 50, 30, 35, 45];
     const random = Math.floor(Math.random() * sizes.length);
-    size += sizes[random]
+    size += sizes[random];
     yes_button.style.height = `${size}px`;
     yes_button.style.width = `${size}px`;
     let total = answers_no[language].length;
-    // change button text
     if (i < total - 1) {
         no_button.innerHTML = answers_no[language][i];
         i++;
@@ -96,20 +75,16 @@ no_button.addEventListener('click', () => {
 });
 
 yes_button.addEventListener('click', () => {
-    // change banner gif path
     let banner = document.getElementById('banner');
     banner.src = "public/images/yes.gif";
     refreshBanner();
-    // hide buttons div
     let buttons = document.getElementsByClassName('buttons')[0];
     buttons.style.display = "none";
-    // show message div
     let message = document.getElementsByClassName('message')[0];
     message.style.display = "block";
 });
 
 function refreshBanner() {
-    // Reload banner gif to force load  
     let banner = document.getElementById('banner');
     let src = banner.src;
     banner.src = '';
@@ -121,32 +96,24 @@ function changeLanguage() {
     const selectedLanguage = selectElement.value;
     language = selectedLanguage;
 
-    // Update question heading
     const questionHeading = document.getElementById("question-heading");
-    if (language === "french") {
-        questionHeading.textContent = "Tu veux être mon valentin?";
-    } else if (language === "thai") {
-        questionHeading.textContent = "คืนดีกับเราได้อ่ะป่าว?";
+    if (language === "japanese") {
+        questionHeading.textContent = "私のバレンタインになってくれますか?";
     } else {
         questionHeading.textContent = "Will you be my valentine?";
     }
 
-    // Reset yes button text
     yes_button.innerHTML = answers_yes[language];
 
-    // Reset button text to first in the new language
     if (clicks === 0) {
         no_button.innerHTML = answers_no[language][0];
     } else {
         no_button.innerHTML = answers_no[language][clicks];
     }
 
-    // Update success message
     const successMessage = document.getElementById("success-message");
-    if (language === "french") {
-        successMessage.textContent = "Yepppie, à bientôt :3";
-    } else if (language === "thai") {
-        successMessage.textContent = "ฮูเร่ คืนดีกันแล้วน้า :3";
+    if (language === "japanese") {
+        successMessage.textContent = "やったー、またすぐ会いましょう :3";
     } else {
         successMessage.textContent = "Yepppie, see you sooonnn :3";
     }
